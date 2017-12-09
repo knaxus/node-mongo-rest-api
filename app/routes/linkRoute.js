6e27c3fe5558abf0cc3a91ef89958b6794bd345a
Priyanka Negi <prinegi100@gmail.com>
2017-12-09 16:13:18 +0530

    completed /DELETE

--- app/routes/linkRoute.js
+++ app/routes/linkRoute.js
@@ -27,7 +27,7 @@ LinkRoute.route('/')
     //validations
     req.check('linkName', 'link name is required').exists().isAlpha().isLength({min:5});
     req.check('linkAdd', 'link address is required').exists();    
-    
+
     let linkData = new Link({
       linkName: req.body.linkName,
       linkAdd: req.body.linkAdd
@@ -50,6 +50,44 @@ LinkRoute.route('/')
         message: 'something went wrong'
       })
     })
-  })
+  });
 
+LinkRoute.route('/:id')
+  .get((req, res) => {
+    req.check('id', 'id is required').exists().isInt();    
+    const { id } = req.params;
+    Link.findById( id )
+    .then((data) => {
+      return res.status(200).json({
+        data,
+        status: 'success',
+        message: 'Get data successfully'
+      })
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  })
+  .delete((req, res) => {
+    //validate id
+    req.check('id', 'id is required').exists().isInt();
+    const { id } = req.params;
+    Link.findByIdAndRemove( id )
+    .then((response) => {
+      // console.log(response);
+      return res.status(200).json({
+        data: [],
+        status: 'success',
+        message: 'deleted data successfully'
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+      return res.status(500).json({
+        data: [],
+        status: 'fail',
+        message: 'something went wrong'
+      })
+    })
+  })
 module.exports = LinkRoute;
