1508f93b65a1e633c55ba5190083c1c8e76a00d4
Ashok Dey <ashokdey100@gmail.com>
2017-12-09 23:43:06 +0530

    --update : setup for coverage using Istanbul

--- app/config/config.js
+++ app/config/config.js
@@ -3,7 +3,7 @@
 const env = process.env.NODE_ENV || 'development';
 
 if (env === 'development') {
-  process.env.PORT = 3000;
+  process.env.PORT = 5000;
   process.env.MONGODB_URI = 'mongodb://localhost/LinkBin';
 } else if (env === 'test') {
   process.env.PORT = 3000;
