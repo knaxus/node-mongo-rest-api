7fe7086e46e0fcacc3ee3e68ea1edacd3aedc263
Priyanka Negi <prinegi100@gmail.com>
2017-12-09 16:51:42 +0530

    convert promise to async-await

--- app/routes/linkRoute.js
+++ app/routes/linkRoute.js
@@ -4,64 +4,60 @@ const bodyParser = require('body-parser');
 const sendResponse = require('../helpers/sendResponse');
 
 LinkRoute.route('/')
-  .get((req, res) => {
-    Link.find()
-    .then((data) => {
-      // console.log(data);
-      return sendResponse(res, 200, data, 'Get data successfully');
-    })
-    .catch((err) => {
+  .get(async (req, res) => {
+    try{
+      const data = await Link.find();
+      return sendResponse(res, 200, data, 'Get data successfully');      
+    }
+    catch(err){
       console.log(err);
-      return sendResponse(res, 404, [], 'data not found');      
-    })
+      return sendResponse(res, 404, [], 'data not found');    
+    }
   })
-  .post((req, res) => {
+  .post(async (req, res) => {
     // const {linkName, linkAdd} = req.body;
     //validations
     req.check('linkName', 'link name is required').exists().isAlpha().isLength({min:5});
-    req.check('linkAdd', 'link address is required').exists();    
-
-    let linkData = new Link({
-      linkName: req.body.linkName,
-      linkAdd: req.body.linkAdd
-    });
-
-    linkData.save()
-    .then((response) => {
-      // console.log(response);
+    req.check('linkAdd', 'link address is required').exists(); 
+    
+    try{
+       const linkData = new Link({
+        linkName: req.body.linkName,
+        linkAdd: req.body.linkAdd
+      });
+      await linkData.save();
       return sendResponse(res, 200, [], 'data saved successfully'); 
-    })
-    .catch((err) => {
+    }
+    catch(err){
       console.log(err);
       return sendResponse(res, 500, [], 'something went wrong'); 
-    })
+    }
   });
 
 LinkRoute.route('/:id')
-  .get((req, res) => {
-    req.check('id', 'id is required').exists().isInt();    
-    const { id } = req.params;
-    Link.findById( id )
-    .then((data) => {
-      return sendResponse(res, 200, data, 'Get data successfully');       
-    })
-    .catch((err) => {
+  .get(async (req, res) => {
+    req.check('id', 'id is required').exists().isInt(); 
+    try{
+      const { id } = req.params;
+      const data = await Link.findById(id);
+      return sendResponse(res, 200, data, 'Get data successfully');      
+    }   
+    catch(err){
       console.log(err);
-      return sendResponse(res, 400, [], 'bad request');       
-    })
+      return sendResponse(res, 400, [], 'bad request');
+    }
   })
-  .delete((req, res) => {
+  .delete(async (req, res) => {
     //validate id
     req.check('id', 'id is required').exists().isInt();
-    const { id } = req.params;
-    Link.findByIdAndRemove( id )
-    .then((response) => {
-      // console.log(response);
-      return sendResponse(res, 200, [], 'deleted data successfully');       
-    })
-    .catch((err) => {
+    try{
+      const { id } = req.params;  
+      await Link.findByIdAndRemove(id);
+      return sendResponse(res, 200, [], 'deleted data successfully');             
+    }
+    catch(err){
       console.log(err)
-      return sendResponse(res, 500, [], 'something went wrong');  
-    })
+      return sendResponse(res, 500, [], 'something went wrong'); 
+    }
   })
 module.exports = LinkRoute;
