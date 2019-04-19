72e466808b60cce6cd6bbe6de174d23a3b5eb7a5
TheJSGirl <prinegi100@gmail.com>
2019-04-19 23:13:48 +0530

    -removed consoles & created ReadMe.md

--- app/config/config.js
+++ app/config/config.js
@@ -9,6 +9,3 @@ if (env === 'development') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost/LinkBin';
 }
-
-// console.log('****Working Environment is : ', env);
-// console.log('****MONGO Environment is : ', process.env.MONGODB_URI);
